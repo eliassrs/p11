59487796018d8d50bca246af9c51e344e1b00241
Luciano <lucianovilela@fai-mg.br>
2021-03-28 16:08:31 -0300

    first

---
+++ src/p11/Main.java
@@ -0,0 +1,18 @@
+package p11;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Main app = new Main();
+		app.start();
+	}
+
+	private void start() {
+
+		int idade = 1;
+		System.out.println("Idade da clarinha: " + idade);
+
+	}
+	
+}
